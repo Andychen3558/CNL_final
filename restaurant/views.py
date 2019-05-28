c178c0d0ddee0f123b1835320e021b76596f48f8
廖威仲 <michael07031224@gmail.com>
2019-05-28 21:27:33 +0800

    rteam ui/user_authenticate

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,26 +1,41 @@
 from django.shortcuts import render, render_to_response, redirect
 from .models import Restaurant
+from django.contrib.auth.decorators import login_required
+from datetime import timedelta
 # Create your views here.
 
+@login_required
 def addRestaurant(request):
 	if request.method == 'GET':
 		return render(request, 'restaurant/addRestaurant.html')
 	elif request.method == 'POST':
-		# print('!!!!!!', request.POST['restaurant_name'])
-		restaurant_instance = Restaurant.objects.create(
-									restaurant_name=request.POST['restaurant_name'],
-									# phone_number=request.POST['phoneNumber'],
-								)
-		restaurant_instance.save()
+		restaurant_instance = Restaurant()
+		try:
+			restaurant_instance.restaurant_name = request.POST['restaurant_name']
+			restaurant_instance.phone_number = request.POST['phone_number']
+			restaurant_instance.seat_number = request.POST['seat_number']
+			restaurant_instance.timeout = timedelta(minutes=int(request.POST['timeout']))
+			restaurant_instance.eat_time = timedelta(minutes=int(request.POST['eat_time']))
+
+			if restaurant_instance.restaurant_name == None or \
+			restaurant_instance.phone_number == None or \
+			restaurant_instance.seat_number == None or \
+			restaurant_instance.timeout == None or \
+			restaurant_instance.eat_time == None:
+				print("Some block are blank.")
+
+			restaurant_instance.save()
+		except Exception as e:
+			print(e)
 		restaurants = Restaurant.objects.all()
 		context = {'restaurants':restaurants}
 		return redirect('/restaurant/listRestaurant/')
-		# return render(request, 'restaurant/listRestaurant.html', context)
 	else:
 		restaurants = Restaurant.objects.all()
 		context = {'restaurants':restaurants}
 		return render(request, 'restaurant/listRestaurant.html', context)
 
+@login_required
 def listRestaurant(request):
 	restaurants = Restaurant.objects.all()
 	context = {'restaurants':restaurants}
