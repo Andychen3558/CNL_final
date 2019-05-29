faff383b189f16b5981aa4a072103c9d59797d34
Andy Chen <b05902058@ntu.edu.tw>
2019-05-29 17:03:44 +0800

    qq

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
+from datetime import timedelta
 
 # Create your models here.
 class CustomUser(AbstractUser):
 	phone_regex = RegexValidator(regex=r'^((?=(09))[0-9]{10})$', message="Phone number must be entered in the format: '09xxxxxxxx'")
 	phone_number = models.CharField(validators=[phone_regex], max_length=17, help_text='Required. Inform a valid phone number.') # validators should be a list
+	arriving_time = models.DurationField(default=timedelta(minutes=5))
 	
 	def __str__(self):
 		return self.username
