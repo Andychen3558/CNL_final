dba1ad627f3076f7f4ad66afc1545357e642bdd4
張凱程 <albert870102@gmail.com>
2019-05-29 16:56:27 +0800

    add arrival time

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.core.validators import RegexValidator
 class CustomUser(AbstractUser):
 	phone_regex = RegexValidator(regex=r'^((?=(09))[0-9]{10})$', message="Phone number must be entered in the format: '09xxxxxxxx'")
 	phone_number = models.CharField(validators=[phone_regex], max_length=17, help_text='Required. Inform a valid phone number.') # validators should be a list
+	arrival_time = models.DurationField(blank=True, verbose_name='Arrival time')
 	
 	def __str__(self):
 		return self.username
