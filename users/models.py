a9403833d64a54f44c727961912f5609f34ddca6
張凱程 <albert870102@gmail.com>
2019-06-08 20:29:54 +0800

    final

--- users/models.py
+++ users/models.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 class CustomUser(AbstractUser):
 	phone_regex = RegexValidator(regex=r'^((?=(09))[0-9]{10})$', message="Phone number must be entered in the format: '09xxxxxxxx'")
 	phone_number = models.CharField(validators=[phone_regex], max_length=17, help_text='Required. Inform a valid phone number.') # validators should be a list
+	#reserve_restaurant = models.ForeignKey(Dicty, db_index=True)
 	arrival_time = models.DurationField(null=True, blank=True, default=timedelta(minutes=5),verbose_name='Arrival time')
 	
 	def __str__(self):
